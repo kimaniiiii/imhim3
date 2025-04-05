a4310fa55a5fabea48d1c54eeb2cd08b8d13323d
Mark Kimani <keamkim06@gmail.com>
2025-04-05 12:02:18 +0000

    increment

--- imhim3/src/counter.jsx
+++ imhim3/src/counter.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-
+import './App.css';
 function Counter(){
     const [count, setCount] = useState(0);
 
 
     return(
         <div>
-            <p>
-                You clicked {count} times
-                <button onClick={() => setCount(count + 1)}> Click me !</button>
-            </p>
-        </div>
+        <h2>My Count is: {count}</h2>
+            <button onClick={() => setCount(count + 1)}> Increment </button>
+            <button onClick={() => setCount(count - 1)}> Decrement </button>
+            <button onClick={() => setCount(0)}>Reset</button> 
+    </div>
     ) ;
 }
 
